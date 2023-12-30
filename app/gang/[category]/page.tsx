7db0c84d23e266ac416d0d185e5ece31a05f57e7
WoutPaepenUcLL <Wout.Paepen@student.ucll.be>
2023-12-30 18:24:25 +0100

    Add activeGerecht state and handle item click event

--- app/gang/[category]/page.tsx
+++ app/gang/[category]/page.tsx
@@ -5,6 +5,7 @@ import React, { useEffect } from 'react';
 
 const Index = ({ params }: { params: { category: string } }) => {
   const [allergenen, setAllergenen] = React.useState<any[] | null>(null);
+  const [activeGerecht, setActiveGerecht] = React.useState<any | null>(null); 
   const router = useRouter();
 
   useEffect(() => {
@@ -13,12 +14,16 @@ const Index = ({ params }: { params: { category: string } }) => {
       setAllergenen(response!);
       console.log(response);
     };
+    setActiveGerecht(null)
     fetchAllergenen();
   }, []);
 
   const handleTitleClicked = () => {
     router.push('/');
   };
+  const handleitemClicked = (allergie: any) => {
+    setActiveGerecht(allergie)
+  }
 
   return (
     <main className="bg-gray-900 text-white min-h-screen p-8 font-mono">
@@ -31,9 +36,16 @@ const Index = ({ params }: { params: { category: string } }) => {
       
 
      <p>{window.screen.width}</p>
+     {activeGerecht?<div className=" mb-4">
+      {/* a x button to set activeGerecht to null */}
+  
+      <p className="activeGerecht">{`${activeGerecht.name}: ${activeGerecht.description}`}</p>
+      <button onClick={()=>setActiveGerecht(null)} className="mb-2 p-2 rounded-md absolute self-end">X</button>
+      </div>:null
+      }
 
       <div className="flex flex-col justify-center mt-8">
-        {window.screen.width>=400?<div className="flex justify-center mb-4">
+        {window.screen.width>=600?<div className="flex justify-center mb-4">
           <table className="min-w-full border border-gray-300 shadow-sm rounded-lg overflow-hidden">
             <thead className="bg-gray-800 text-white">
               <tr>
@@ -53,7 +65,27 @@ const Index = ({ params }: { params: { category: string } }) => {
               ))}
             </tbody>
           </table>
-        </div>:null}
+        </div>:<div className="flex justify-center mb-4">
+          <table className="min-w-full border border-gray-300 shadow-sm rounded-lg overflow-hidden">
+            <thead className="bg-gray-800 text-white">
+              <tr>
+                <th className="px-6 py-3 text-left">Naam</th>
+                <th className="px-6 py-3 text-left">Beschrijving</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allergenen?.map((allergie) => (
+                <tr
+                  key={allergie.name}
+                  className="hover:bg-gray-100 hover:text-black transition duration-300"
+                >
+                  <td className="border px-6 py-4">{allergie.name}</td>
+                  <td onClick={()=>handleitemClicked(allergie)} className="border px-6 py-4 hover:bg-slate-300 hover:text-black">klik voor uitleg gerecht</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>}
         <h2 className="text-2xl font-semibold mb-2 text-center">Allergie-informatie</h2>
         
         <table className="min-w-full border border-gray-300 shadow-sm rounded-lg overflow-hidden">
