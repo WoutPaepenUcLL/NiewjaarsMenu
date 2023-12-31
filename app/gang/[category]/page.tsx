215213d02c4348402b14ab3628bd2d2115b434be
WoutPaepenUcLL <Wout.Paepen@student.ucll.be>
2023-12-31 09:51:07 +0100

    Refactor page.tsx to render strong text in activeGerecht

--- app/gang/[category]/page.tsx
+++ app/gang/[category]/page.tsx
@@ -24,9 +24,12 @@ const Index = ({ params }: { params: { category: string } }) => {
   const handleitemClicked = (allergie: any) => {
     setActiveGerecht(allergie)
   }
+  const renderStrong = (text: any) => {
+    return <span><strong>{`${text.name}: `}</strong>{text.description}</span>;
+  }
 
   return (
-    <main className="bg-gray-900 text-white min-h-screen p-8 font-mono">
+    <main className="bg-gray-900 text-white min-h-screen font-mono select-none">
       <h1
         onClick={handleTitleClicked}
         className="text-5xl font-bold mb-4 text-center cursor-pointer"
@@ -36,16 +39,16 @@ const Index = ({ params }: { params: { category: string } }) => {
       
 
 
-     {activeGerecht?<div className="m-0">
+     {activeGerecht?<div className="m-0 top-50 fixed z-5 w-full">
       {/* a x button to set activeGerecht to null */}
   
-      <p className="activeGerecht">{`${activeGerecht.name}: ${activeGerecht.description}`}</p>
+      <p className="activeGerecht">{renderStrong(activeGerecht)}</p>
       <button onClick={()=>setActiveGerecht(null)} className="mb-2 p-2 rounded-md absolute self-end">X</button>
       </div>:null
       }
 
-      <div className="flex flex-col justify-center mt-8">
-        {window.screen.width>=600?<div className="flex justify-center mb-4">
+      <div className="z-0 flex flex-col justify-center mt-8">
+        {window.screen.width>=600?<div className="flex justify-center mb-4 z-1">
           <table className="min-w-full border border-gray-300 shadow-sm rounded-lg overflow-hidden">
             <thead className="bg-gray-800 text-white">
               <tr>
